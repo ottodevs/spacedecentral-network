fad6d11d9692e87cac38824aba5776dff6a15d83
Otto G <ottodevs@gmail.com>
2018-05-27 01:47:49 +0200

    Setup devise spec config and do some sample testing. It should be noted that some devise methods are overriden in user model, so we changed that before try to testing some devise mailing, like confirmation emails.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
 require 'rspec/rails'
+require 'devise'
 require 'support/factory_bot'
 require 'support/mailer'
 require 'capybara/rails'
